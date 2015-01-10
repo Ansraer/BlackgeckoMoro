039f8ae0d6e8f4be5a37300dd583ecc7433a56f1
AruIke <marcus.haubert@gmail.com>
2015-01-10 12:54:42 +0100

    commando: help

--- src/main/java/com/blackgecko/moro/mod/Moro.java
+++ src/main/java/com/blackgecko/moro/mod/Moro.java
@@ -1,6 +1,8 @@
 package com.blackgecko.moro.mod;
 
 import com.blackgecko.moro.mod.commands.CommandInfo;
+import com.blackgecko.moro.mod.commands.MoroHelp;
+import com.blackgecko.moro.mod.events.MoroEventHandler;
 
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraft.init.Blocks;
@@ -14,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 @Mod(modid = Moro.MODID, version = Moro.VERSION, acceptableRemoteVersions = "*")
 public class Moro
 {
-    public static final String MODID = "moror";
+    public static final String MODID = "moro";
     public static final String VERSION = "1.0";
     @Instance(MODID)
 	public static Moro instance;
@@ -25,6 +27,7 @@ public class Moro
     public void serverLoad(FMLServerStartingEvent event)
     {
       event.registerServerCommand(new CommandInfo());
+      event.registerServerCommand(new MoroHelp());
     }
     
     @EventHandler
