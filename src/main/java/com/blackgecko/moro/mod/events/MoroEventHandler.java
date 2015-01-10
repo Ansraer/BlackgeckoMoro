c8237e7308eb97ee54f581ac3c76d9ad22cd0aed
Jacky2611 <jacky2611@gmail.com>
2015-01-10 13:12:03 +0100

    events

--- src/main/java/com/blackgecko/moro/mod/events/MoroEventHandler.java
+++ src/main/java/com/blackgecko/moro/mod/events/MoroEventHandler.java
@@ -1,5 +1,6 @@
 package com.blackgecko.moro.mod.events;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
@@ -8,7 +9,7 @@ public class MoroEventHandler {
 	
 	@SubscribeEvent
 	public void onPlayerTickEvent(PlayerTickEvent event){
-
+		EntityPlayer player = event.player;
 	}
 	
 
